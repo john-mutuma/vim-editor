ec5fa0a88b34c3a0ed9507eb443c72fe2443f199
John Mutuma <johnmutuma@microsoft.com>
2024-11-24 18:39:39 +0300

    Fix colorscheme switch issue for glance
    Set autocommand to override Glance.nvim highlights groups

--- v2/nvim/lua/johnmutuma/plugins/glance.lua
+++ v2/nvim/lua/johnmutuma/plugins/glance.lua
@@ -10,36 +10,49 @@ glance.setup({
 	},
 })
 
-vim.cmd([[
-  :hi GlanceBorderTop gui=underline guifg=#ededed guibg=#10110A
+local function setHiglightOverrides()
+	if opt.background:get() == "light" then
+		print("Glance: Setting highlight overrides for light theme")
+		vim.cmd([[
+      :hi GlanceBorderTop gui=underline guifg=black guibg=white
 
-  :hi GlanceWinBarFilepath gui=italic guibg=#14140F
-  :hi GlanceWinBarFilename gui=none guibg=#14140F
-  :hi GlanceWinBarTitle gui=none guibg=#14140F
+      :hi GlanceWinBarFilepath gui=italic guibg=#c1c1c1
+      :hi GlanceWinBarFilename gui=italic guifg=black guibg=#c1c1c1
+      :hi GlanceWinBarTitle gui=bold guifg=#010101 guibg=#c1c1c1
 
-  :hi GlancePreviewBorderBottom gui=underline guifg=#eeeeee
+      :hi GlancePreviewBorderBottom gui=underline guifg=black
 
-  :hi GlanceListCursorLine gui=none guibg=#656661
+      :hi GlanceListCursorLine gui=none guibg=#aaaaaa
 
-  :hi GlanceListBorderBottom gui=underline guifg=#eeeeee "guibg=#1D1D1D
-  :hi GlanceListNormal gui=italic guifg=e1e1e1
-  " :hi GlanceListEndOfBuffer gui=none guibg=#1D1D1D
-]])
+      :hi GlanceListBorderBottom gui=underline guifg=black"guibg=#1D1D1D
+      :hi GlanceListNormal gui=italic guifg=indigo
+      " :hi GlanceListEndOfBuffer gui=underline guibg=#1D1D1D
+    ]])
+	else
+		print("Glance: Setting highlight overrides for dark theme")
+		vim.cmd([[
+      :hi GlanceBorderTop gui=underline guifg=#ededed guibg=#10110A
 
-if opt.background:get() == "light" then
-	vim.cmd([[
-  :hi GlanceBorderTop gui=underline guifg=black guibg=white
+      :hi GlanceWinBarFilepath gui=italic guibg=#14140F
+      :hi GlanceWinBarFilename gui=none guibg=#14140F
+      :hi GlanceWinBarTitle gui=none guibg=#14140F
 
-  :hi GlanceWinBarFilepath gui=italic guibg=#c1c1c1
-  :hi GlanceWinBarFilename gui=italic guifg=black guibg=#c1c1c1
-  :hi GlanceWinBarTitle gui=bold guifg=#010101 guibg=#c1c1c1
+      :hi GlancePreviewBorderBottom gui=underline guifg=#eeeeee
 
-  :hi GlancePreviewBorderBottom gui=underline guifg=black
+      :hi GlanceListCursorLine gui=none guibg=#656661
 
-  :hi GlanceListCursorLine gui=none guibg=#aaaaaa
-
-  :hi GlanceListBorderBottom gui=underline guifg=black"guibg=#1D1D1D
-  :hi GlanceListNormal gui=italic guifg=indigo
-  " :hi GlanceListEndOfBuffer gui=underline guibg=#1D1D1D
-]])
+      :hi GlanceListBorderBottom gui=underline guifg=#eeeeee "guibg=#1D1D1D
+      :hi GlanceListNormal gui=italic guifg=e1e1e1
+      " :hi GlanceListEndOfBuffer gui=none guibg=#1D1D1D
+    ]])
+	end
 end
+
+setHiglightOverrides()
+
+local colorschemechange = vim.api.nvim_create_augroup("colorschemechange", { clear = true })
+vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+	group = "colorschemechange",
+	callback = setHiglightOverrides,
+	-- pattern = {"*.adoc", "*.md", "*.tex"},
+})
