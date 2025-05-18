38ff9c2768ed492946ca537e0d6cd292a1753162
John Mutuma <johnmutuma5@gmail.com>
2025-05-18 15:32:08 +0000

    feat(plugins): restructure and add which-key.nvim
    Refactored plugin configuration for improved organization by grouping
    plugins into categories: Colorschemes, Editing Enhancements, UI/UX,
    Search/Replace, and Markdown. Added 'folke/which-key.nvim' for enhanced
    keybinding discovery and lazy-loaded it for performance. Moved
    'MagicDuck/grug-far.nvim' to its own Search/Replace section. Updated
    plugin loading strategies and initialization functions for clarity.

--- v2/nvim/lua/johnmutuma/plugins/init.lua
+++ v2/nvim/lua/johnmutuma/plugins/init.lua
@@ -7,59 +7,73 @@ local keymap = vim.keymap
 globl.sonokai_enable_italic = true
 
 return {
-    --  colorschemes
-    -- { "ellisonleao/gruvbox.nvim" },
-    -- { "Mofiqul/vscode.nvim" },
-    -- { "folke/tokyonight.nvim" },
-    -- { "catppuccin/nvim",         name = "catppuccin" },
+    -- Colorschemes
     {
-        "Shatur/neovim-ayu",
-        priority = 1000,
-        config = function()
-            keymap.set("n", "<leader>DD", ":colorscheme ayu-mirage<CR>")
-            keymap.set("n", "<leader>LL", ":colorscheme dayfox<CR>")
-            vim.cmd("colorscheme ayu-mirage")
-        end,
+        {
+            "Shatur/neovim-ayu",
+            priority = 1000,
+            init = function()
+                keymap.set("n", "<leader>DD", ":colorscheme ayu-mirage<CR>")
+                keymap.set("n", "<leader>LL", ":colorscheme dayfox<CR>")
+                vim.cmd("colorscheme ayu-mirage")
+            end,
+        },
+        { "EdenEast/nightfox.nvim", event = { "VeryLazy" } },
+        -- { "ellisonleao/gruvbox.nvim" },
+        -- { "Mofiqul/vscode.nvim" },
+        -- { "folke/tokyonight.nvim" },
+        -- { "catppuccin/nvim",         name = "catppuccin" },
     },
-    { "EdenEast/nightfox.nvim",                event = { "VeryLazy" } },
-    --
-    --
+
+    -- Editing Enhancements
     {
-        "MagicDuck/grug-far.nvim",
-        -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
-        -- additional lazy config to defer loading is not really needed...
-        config = function()
-            -- optional setup call to override plugin options
-            -- alternatively you can set options with vim.g.grug_far = { ... }
-            require("grug-far").setup({
-                -- options, see Configuration section below
-                -- there are no required options atm
-            })
-        end,
+        { "sindrets/diffview.nvim" },
+        { "mattn/emmet-vim" },
+        { "unblevable/quick-scope" },
+        { "junegunn/vim-peekaboo" },
+        { "ctrlpvim/ctrlp.vim" },
+        { "windwp/nvim-ts-autotag", event = { "BufReadPre", "BufNewFile" } },
+        { "tpope/vim-surround" },
     },
-    { "sindrets/diffview.nvim" },
-    { "mattn/emmet-vim" },
-    { "unblevable/quick-scope" },
-    { "junegunn/vim-peekaboo" },
-    { "ctrlpvim/ctrlp.vim" },
-    { "windwp/nvim-ts-autotag" },
-    { "tpope/vim-surround" },
-    { "vim-scripts/ReplaceWithRegister" },
-    { "machakann/vim-highlightedyank" },
+
+    -- UI/UX
     {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-    },
-    {
-        "folke/zen-mode.nvim",
-        opts = {
-            window = {
-                width = 200,
+        {
+            "folke/which-key.nvim",
+            event = "VeryLazy",
+            opts = {
+                delay = 700,
             },
+        },
+        {
+            "folke/zen-mode.nvim",
+            opts = {
+                window = {
+                    width = 200,
+                },
+            },
+        },
+    },
+
+    -- Search/Replace
+    {
+        {
+            "MagicDuck/grug-far.nvim",
+            config = function()
+                require("grug-far").setup({})
+            end,
+        },
+    },
+
+    -- Markdown
+    {
+        {
+            "iamcco/markdown-preview.nvim",
+            cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+            ft = { "markdown" },
+            build = function()
+                vim.fn["mkdp#util#install"]()
+            end,
         },
     },
 }
