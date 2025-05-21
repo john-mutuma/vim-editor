d71542a9771db16317e8c14cc19a5c94abf8c064
John Mutuma <johnmutuma5@gmail.com>
2025-05-21 15:21:16 +0000

    feat(gitsigns): add custom commands for which-key support
    - Introduced user commands `GitsignsBlameLine` and `GitsignsBlameLineFull`
      to enable better integration with the which-key plugin.
    - Refactored key mappings to use these commands, allowing which-key to
      display descriptive labels for gitsigns actions.
    - Updated mappings to use <cmd> for improved compatibility and clarity.

--- v2/nvim/lua/johnmutuma/plugins/gitsigns.lua
+++ v2/nvim/lua/johnmutuma/plugins/gitsigns.lua
@@ -2,6 +2,16 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
+        vim.api.nvim_create_user_command("GitsignsBlameLine", function()
+            local gitsigns = require("gitsigns")
+            gitsigns.blame_line()
+        end, { desc = "Toggle blame line" })
+
+        vim.api.nvim_create_user_command("GitsignsBlameLineFull", function()
+            local gitsigns = require("gitsigns")
+            gitsigns.blame_line()
+        end, { desc = "Toggle full blame line" })
+
         require("gitsigns").setup({
             preview_config = {
                 border = "single",
@@ -37,31 +47,29 @@ return {
                 end, { expr = true })
 
                 -- Actions
-                map("n", "<leader>hs", gs.stage_hunk)
-                map("n", "<leader>hr", gs.reset_hunk)
+                map("n", "<leader>hs", ":Gitsigns stage_hunk<CR>")
+                map("n", "<leader>hr", ":Gitsigns reset_hunk<CR>")
                 map("v", "<leader>hs", function()
                     gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
                 end)
                 map("v", "<leader>hr", function()
                     gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
                 end)
-                map("n", "<leader>hS", gs.stage_buffer)
-                map("n", "<leader>hu", gs.undo_stage_hunk)
-                map("n", "<leader>hR", gs.reset_buffer)
-                map("n", "<leader>hp", gs.preview_hunk)
-                map("n", "<leader>hb", gs.blame_line)
-                map("n", "<leader>hB", function()
-                    gs.blame_line({ full = true })
-                end)
-                map("n", "<leader>tb", gs.toggle_current_line_blame)
-                map("n", "<leader>hd", gs.diffthis)
+                map("n", "<leader>hS", "<cmd>Gitsigns stage_buffer<CR>")
+                map("n", "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>")
+                map("n", "<leader>hR", "<cmd>Gitsigns reset_buffer<CR>")
+                map("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>")
+                map("n", "<leader>hb", "<cmd>GitsignsBlameLine<CR>")
+                map("n", "<leader>hB", "<cmd>GitsignsBlameLineFull<CR>")
+                map("n", "<leader>htb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
+                map("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>")
                 map("n", "<leader>hD", function()
                     gs.diffthis("~")
                 end)
-                map("n", "<leader>td", gs.toggle_deleted)
+                map("n", "<leader>td", "<cmd>Gitsigns toggle_deleted<CR>")
 
                 -- Text object
-                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+                map({ "o", "x" }, "ih", "<cmd><C-U>Gitsigns select_hunk<CR>")
             end,
         })
     end,
