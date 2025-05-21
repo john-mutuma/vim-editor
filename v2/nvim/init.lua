20a2f50db914a55ae2332a49c9c78939f22c308d
John Mutuma <johnmutuma5@gmail.com>
2025-05-21 15:46:53 +0000

    feat(init): add autosave autocmd (commented out)
    Added a commented-out autocmd for autosaving files on InsertLeave and
    FocusLost events. This prepares for future autosave functionality,
    allowing easy activation when needed. The autocmd checks if the buffer
    is modified, has a filetype, and is associated with a file before
    writing and printing a confirmation message.

--- v2/nvim/init.lua
+++ v2/nvim/init.lua
@@ -1,2 +1,15 @@
 require("johnmutuma.core")
 require("johnmutuma.lazy")
+
+-- Autosave on InsertLeave and FocusLost
+-- TODO: add this auto save functionality when needed and ready
+-- vim.api.nvim_create_autocmd({ "InsertLeave", "FocusLost" }, {
+--     pattern = "*",
+--     callback = function()
+--         if vim.bo.modified and vim.bo.filetype ~= "" and vim.fn.expand("%") ~= "" then
+--             vim.cmd("silent! write")
+--             print("Autosaved ")
+--         end
+--     end,
+--     desc = "Autosave on insert leave or focus lost",
+-- })
