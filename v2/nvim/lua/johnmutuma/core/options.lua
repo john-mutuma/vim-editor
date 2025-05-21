8ffd57c386c8a94227c1a9c4fe862e5d6e6885ef
John Mutuma <johnmutuma5@gmail.com>
2025-05-21 08:09:42 +0000

    (misc): formatting

--- v2/nvim/lua/johnmutuma/core/options.lua
+++ v2/nvim/lua/johnmutuma/core/options.lua
@@ -61,4 +61,4 @@ globl.qs_highlight_on_keys = { "f", "F", "t", "T", "/", "?" }
 
 -- " Enable blinking together with different cursor shapes for insert/command mode, and cursor highlighting:
 opt.guicursor =
-"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+    "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
