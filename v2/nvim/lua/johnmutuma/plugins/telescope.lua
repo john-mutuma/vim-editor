eb3266bb32424c428bc206d44e8223269ec957ce
John Mutuma <johnmutuma5@gmail.com>
2025-05-25 19:27:59 +0000

    refactor(telescope): organize config with sections
    - Added clear section banners to telescope.lua for improved readability
      and maintainability.
    - Grouped related configuration logic into numbered sections:
      1. Plugin Setup
      2. Module Requires
      3. Telescope Setup
      4. Load Extensions (Safely)
      5. Keymap Helper
      6. Telescope Keymaps
      7. Add Backdrop for Depth
    - Replaced direct keymap calls with a local `map` helper for clarity.
    - Used `pcall` to safely load the fzf extension.
    - Removed commented-out and redundant code for a cleaner config.

--- v2/nvim/lua/johnmutuma/plugins/telescope.lua
+++ v2/nvim/lua/johnmutuma/plugins/telescope.lua
@@ -1,4 +1,6 @@
-local keymap = vim.keymap
+----------------------------------------------------------------------
+-- 1. Telescope Plugin Setup
+----------------------------------------------------------------------
 
 return {
     "nvim-telescope/telescope.nvim",
@@ -8,56 +10,55 @@ return {
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
+        ----------------------------------------------------------------------
+        -- 2. Module Requires
+        ----------------------------------------------------------------------
         local telescope = require("telescope")
         local builtin = require("telescope.builtin")
-
         local window_utils = require("johnmutuma.utils.windows")
+
+        ----------------------------------------------------------------------
+        -- 3. Telescope Setup
+        ----------------------------------------------------------------------
         telescope.setup({
             defaults = {
-                -- Default configuration for telescope goes here:
-                -- config_key = value,
                 mappings = {
                     i = {
-                        -- map actions.which_key to <C-h> (default: <C-/>)
-                        -- actions.which_key shows the mappings for your picker,
-                        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
                         ["<C-?>"] = "which_key",
                         ["<C-j>"] = "move_selection_next",
                         ["<C-k>"] = "move_selection_previous",
                     },
                 },
             },
-            pickers = {
-                -- Default configuration for builtin pickers goes here:
-                -- picker_name = {
-                --   picker_config_key = value,
-                --   ...
-                -- }
-                -- Now the picker_config_key will be applied every time you call this
-                -- builtin picker
-            },
-            extensions = {
-                -- Your extension configuration goes here:
-                -- extension_name = {
-                --   extension_config_key = value,
-                -- }
-                -- please take a look at the readme of the extension you want to configure
-            },
         })
 
-        telescope.load_extension("fzf") -- make use of nvim-fzf-native plugin for better performance
+        ----------------------------------------------------------------------
+        -- 4. Load Extensions (Safely)
+        ----------------------------------------------------------------------
+        pcall(function()
+            telescope.load_extension("fzf")
+        end)
 
-        -- Plugins mappings
-        -- -- telescope
-        keymap.set("n", "<C-F>f", builtin.git_files, {})
-        keymap.set("n", "<C-F>s", builtin.live_grep, {})
-        keymap.set("n", "<C-F>b", builtin.buffers, {})
-        keymap.set("n", "<C-F>r", builtin.oldfiles, {})
-        keymap.set("n", "<C-F>h", builtin.help_tags, {})
-        keymap.set("n", "<C-F>y", builtin.git_branches, {})
+        ----------------------------------------------------------------------
+        -- 5. Keymap Helper
+        ----------------------------------------------------------------------
+        local function map(lhs, func, desc)
+            vim.keymap.set("n", lhs, func, { desc = desc })
+        end
 
-        -- manually adding a backdrop to telescope prompt to add depth
-        -- can remove this when Telescope has added a backdrop internally or if neovim decideds to include backdrops to floating windows for depth
+        ----------------------------------------------------------------------
+        -- 6. Telescope Keymaps
+        ----------------------------------------------------------------------
+        map("<C-F>f", builtin.git_files, "Telescope: Git Files")
+        map("<C-F>s", builtin.live_grep, "Telescope: Live Grep")
+        map("<C-F>b", builtin.buffers, "Telescope: Buffers")
+        map("<C-F>r", builtin.oldfiles, "Telescope: Recent Files")
+        map("<C-F>h", builtin.help_tags, "Telescope: Help Tags")
+        map("<C-F>y", builtin.git_branches, "Telescope: Git Branches")
+
+        ----------------------------------------------------------------------
+        -- 7. Add Backdrop for Depth
+        ----------------------------------------------------------------------
         window_utils.with_win_backdrop("TelescopePrompt")
     end,
 }
