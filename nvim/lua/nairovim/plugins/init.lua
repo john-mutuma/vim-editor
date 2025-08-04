0b61519b3625488bff62ef359aacac69f11d7dda
John Mutuma <johnmutuma@microsoft.com>
2025-08-04 12:04:41 +0000

    refactor: move render-markdown plugin from avante to init
    - Move render-markdown.nvim configuration from ai/avante.lua to plugins/init.lua
    
    - Improves plugin organization by separating markdown rendering from AI-specific configs
    
    - Maintains same functionality with cleaner separation of concerns
    
    Signed-off-by: John Mutuma <johnmutuma@microsoft.com>

--- nvim/lua/nairovim/plugins/init.lua
+++ nvim/lua/nairovim/plugins/init.lua
@@ -80,5 +80,17 @@ return {
                 vim.fn["mkdp#util#install"]()
             end,
         },
+        {
+            -- Make sure to set this up properly if you have lazy=true
+            "MeanderingProgrammer/render-markdown.nvim",
+            opts = {
+                file_types = { "markdown", "Avante", "mcphub", "copilot-chat" },
+                code = {
+                    sign = false,
+                    language_border = "",
+                },
+            },
+            ft = { "markdown", "Avante", "mcphub", "copilot-chat" },
+        },
     },
 }
