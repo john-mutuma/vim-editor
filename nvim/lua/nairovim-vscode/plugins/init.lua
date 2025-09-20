3230305b78b2cb2d7a94373cf45f1099a34f925c
John Mutuma <johnmutuma@microsoft.com>
2025-09-20 21:40:00 +0300

    feat(vscode): add VSCode configuration for NairoVIM
    - Add core configuration with keymaps for VSCode integration
    
    - Set up leader key mappings (space and comma)
    
    - Configure system clipboard operations
    
    - Add VSCode-specific actions (references, inline chat, window management)
    
    - Include lazy.nvim setup for VSCode environment
    
    - Add essential editing plugins (emmet, surround, autotag, etc.)
    
    Signed-off-by: John Mutuma <johnmutuma@microsoft.com>

---
+++ nvim/lua/nairovim-vscode/plugins/init.lua
@@ -0,0 +1,15 @@
+local vim = vim
+
+return {
+    -- Editing Enhancements
+    {
+        { "mattn/emmet-vim" },
+        -- { "unblevable/quick-scope" },
+        -- { "junegunn/vim-peekaboo" },
+        -- { "ctrlpvim/ctrlp.vim" },
+        { "windwp/nvim-ts-autotag" },
+        { "tpope/vim-surround" },
+        { "vim-scripts/ReplaceWithRegister" },
+        { "machakann/vim-highlightedyank" },
+    },
+}
