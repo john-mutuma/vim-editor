c22b6cbfd467029183016b0f32b1a1cde2001487
John Mutuma <johnmutuma@microsoft.com>
2025-10-04 15:29:21 +0300

    refactor(scooter): reorganize keymaps and add user command
    - Created dedicated keymap file following project conventions
    
    - Moved keymaps from inline definitions to customizations/keymaps/scooter.lua
    
    - Added :FindReplace user command for opening scooter terminal
    
    - Updated keymap handlers to use user command for consistency
    
    - Applied keymaps using common_utils.map() following project pattern
    
    - Changed function definitions to use Lua docstring format (---)
    
    Signed-off-by: John Mutuma <johnmutuma@microsoft.com>

--- nvim/lua/nairovim/plugins/scooter.lua
+++ nvim/lua/nairovim/plugins/scooter.lua
@@ -1,8 +1,9 @@
+local M = {}
 local Terminal = require("toggleterm.terminal").Terminal
 local scooter_term = nil
 
--- Open existing scooter terminal if one is available, otherwise create a new one
-local function open_scooter()
+--- Open existing scooter terminal if one is available, otherwise create a new one
+local open_scooter = function()
     if not scooter_term then
         scooter_term = Terminal:new({
             cmd = "scooter",
@@ -22,7 +23,7 @@ local function open_scooter()
     scooter_term:open()
 end
 
--- Called by scooter to open the selected file at the correct line from the scooter search list
+--- Called by scooter to open the selected file at the correct line from the scooter search list
 _G.EditLineFromScooter = function(file_path, line)
     if scooter_term and scooter_term:is_open() then
         scooter_term:close()
@@ -38,7 +39,7 @@ _G.EditLineFromScooter = function(file_path, line)
     vim.api.nvim_win_set_cursor(0, { line, 0 })
 end
 
--- Opens scooter with the search text populated by the `search_text` arg
+--- Opens scooter with the search text populated by the `search_text` arg
 _G.OpenScooterSearchText = function(search_text)
     if scooter_term and scooter_term:is_open() then
         scooter_term:close()
@@ -56,13 +57,16 @@ _G.OpenScooterSearchText = function(search_text)
     scooter_term:open()
 end
 
-vim.keymap.set("n", "<leader>s", open_scooter, { desc = "Open scooter" })
+----------------------------------------------------------------------
+-- 2. User Commands
+----------------------------------------------------------------------
+vim.api.nvim_create_user_command("FindReplace", open_scooter, { desc = "Open scooter terminal for Find and replace" })
 
-vim.keymap.set(
-    "v",
-    "<leader>r",
-    '"ay<ESC><cmd>lua OpenScooterSearchText(vim.fn.getreg("a"))<CR>',
-    { desc = "Search selected text in scooter" }
-)
+----------------------------------------------------------------------
+-- 3. Keymaps
+----------------------------------------------------------------------
+local common_utils = require("nairovim.utils.common")
+local mappings = require("nairovim.plugins.customizations.keymaps.scooter").mappings
+common_utils.map(mappings)
 
-return {}
+return M
